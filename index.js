9d7a12ac6c6e664865f82c193634d930467633e4
leen <programmerleen@gmail.com>
2025-07-08 16:41:37 -0700

    Updated index.js with fetch API logic

--- index.js
+++ index.js
@@ -1,4 +1,53 @@
-function sayHello(name) {
-  console.log("Hello leen " + name);
+const BASE_URL = "https://introbackendcourse.onrender.com/users";
+
+async function getAllUsers() {
+  const res = await fetch(BASE_URL);
+  const data = await res.json();
+  console.log("All users:", data);
+  return data;
 }
-sayHello("Mosh");
+
+async function createUser(userObj) {
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userObj),
+  });
+  const data = await res.json();
+  console.log("Created user:", data);
+}
+
+async function getSecondUser() {
+  const users = await getAllUsers();
+  const secondUser = users[1];
+  console.log("Second user:", secondUser);
+  return secondUser;
+}
+
+async function updateUser(id, updatedInfo) {
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedInfo),
+  });
+  const data = await res.json();
+  console.log("Updated user:", data);
+}
+
+async function deleteUser(id) {
+  await fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+  });
+  console.log(`User with id ${id} deleted`);
+}
+
+const extractNameAndMark = (user) => {
+  return {
+    name: user.name,
+    mark: user.mark,
+  };
+};
